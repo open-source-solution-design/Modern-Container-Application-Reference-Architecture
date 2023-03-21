bb129e4d18993d57c3019fc7bd091f3204ae9385
Haitao Pan <manbuzhe2009@qq.com>
2023-03-21 23:31:31 +0800

    __main__.py: debug

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -11,12 +11,12 @@ from aws import internet_gateway
 from aws import availability_zones
 from aws import key_pair
 
-vpc_id  = vpc()
-az_list = availability_zones()
-sg_id   = security_group( vpc_id )
-igw_id  = internet_gateway( vpc_id )
-route_table_id = route_table( vpc_id, igw_id )
-subnets = subnets(vpc_id, az_list, route_table_id, 'public' )
+#vpc_id  = vpc()
+#az_list = availability_zones()
+#sg_id   = security_group( vpc_id )
+#igw_id  = internet_gateway( vpc_id )
+#route_table_id = route_table( vpc_id, igw_id )
+#subnets = subnets(vpc_id, az_list, route_table_id, 'public' )
 
 ssh_key = config.get('SSH_PUBLIC_KEY')
 print(ssh_key)
