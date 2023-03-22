0638b9490ba8f8dff54a2e55b16af6e1211a1596
shenlan <manbuzhe2009@qq.com>
2023-03-22 19:41:15 +0800

    Update __init__.py

--- infrastructure/aws/__init__.py
+++ infrastructure/aws/__init__.py
@@ -8,17 +8,11 @@ project_name = pulumi.get_project()
 
 #------------------------------------#
 def vpc():
-    vpc = ec2.Vpc(
-            resource_name=f"eks-{project_name}-{stack_name}",
-            cidr_block="10.100.0.0/16",
-            enable_dns_support=True,
-            enable_dns_hostnames=True,
-            instance_tenancy='default',
+    vpc = ec2.Vpc(resource_name=f"eks-{project_name}-{stack_name}", cidr_block="10.100.0.0/16", enable_dns_support=True, enable_dns_hostnames=True, instance_tenancy='default',
             tags={
                 "Project": project_name,
                 "Stack": stack_name
-                }
-            )
+                })
     return vpc.id
 #------------------------------------#
 def key_pair( resource_name: str, public_key: str ):
