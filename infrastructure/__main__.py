39b7d1dbc829c978cba95a996ff964897dffffbd
Haitao Pan <manbuzhe2009@qq.com>
2023-03-22 11:12:44 +0800

    config/__init__.py: add config.get_env()

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -9,23 +9,18 @@ from aws import internet_gateway
 from aws import availability_zones
 from aws import key_pair
 
-
 vpc_id  = vpc()
 az_list = availability_zones()
 sg_id   = security_group( vpc_id )
 igw_id  = internet_gateway( vpc_id )
 route_table_id = route_table( vpc_id, igw_id )
 subnets = subnets(vpc_id, az_list, route_table_id, 'public' )
+
 key_pair_name = key_pair(
         "my_ssh_key",
-        public_key=config.get('SSH_PUBLIC_KEY') 
+        public_key=config.get_env('SSH_PUBLIC_KEY')
     )
 
-# Create an AWS resource (S3 Bucket)
-#bucket = s3.Bucket('my-bucket')
-
-# Export the name of the bucket
-#pulumi.export('bucket_name', bucket)
 pulumi.export("vpc", vpc_id)
 pulumi.export("sg", sg_id)
 pulumi.export("subnets", subnets)
--- infrastructure/config/__init__.py
+++ infrastructure/config/__init__.py
@@ -1,12 +1,13 @@
-import os
-from pulumi import Output
 from pulumi_command import local
 from pulumi_command import remote
 
-def get(input):
-    value = Output.from_input(input)
-    return value.apply( os.environ[input] )
-#-------------------------------------------------------------#
+def get_env( name ):
+    env = local.Command(
+            "command",
+            create=f'printenv {name}'
+            )
+    return env.stdout
+
 def local_run( command: str ):
     command = local.Command("local_command", create=command )
     return command.stdout
