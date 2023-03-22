23eda9edca134b5f313dbfe0b8443c0e8e9e0386
Haitao Pan <manbuzhe2009@qq.com>
2023-03-22 10:50:12 +0800

    config/__init__.py: update

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -1,8 +1,5 @@
 """An AWS Python Pulumi AWS Module"""
-
 import config
-import pulumi
-import pulumi_aws
 
 from aws import vpc
 from aws import subnets
@@ -12,7 +9,6 @@ from aws import internet_gateway
 from aws import availability_zones
 from aws import key_pair
 
-from pulumi_command import local
 
 vpc_id  = vpc()
 az_list = availability_zones()
@@ -20,14 +16,10 @@ sg_id   = security_group( vpc_id )
 igw_id  = internet_gateway( vpc_id )
 route_table_id = route_table( vpc_id, igw_id )
 subnets = subnets(vpc_id, az_list, route_table_id, 'public' )
-
-#ssh_key = local.Command("random",
-#    create="printenv SSH_PUBLIC_KEY"
-#)
-
-public_key=config.get('SSH_PUBLIC_KEY')
-key_pair = key_pair("deployer", public_key)
-
+key_pair_name = key_pair(
+        "my_ssh_key",
+        public_key=config.get('SSH_PUBLIC_KEY') 
+    )
 
 # Create an AWS resource (S3 Bucket)
 #bucket = s3.Bucket('my-bucket')
@@ -37,4 +29,4 @@ key_pair = key_pair("deployer", public_key)
 pulumi.export("vpc", vpc_id)
 pulumi.export("sg", sg_id)
 pulumi.export("subnets", subnets)
-pulumi.export("keypair", key_pair.key_name)
+pulumi.export("keypair", key_pair_name)
--- infrastructure/config/__init__.py
+++ infrastructure/config/__init__.py
@@ -1,6 +1,12 @@
 import os
 from pulumi import Output
+from pulumi_command import local
+from pulumi_command import remote
 
 def get(input):
     value = Output.from_input(input)
-    return value.apply( os.environ[key_name] )
+    return value.apply( os.environ[input] )
+#-------------------------------------------------------------#
+def local_run( command: str ):
+    command = local.Command("local_command", create=command )
+    return command.stdout
