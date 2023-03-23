918bc99f2ea45625bb4789bcd1d6b131f8f788a5
Haitao Pan <manbuzhe2009@qq.com>
2023-03-23 14:25:29 +0800

    fixed setup resources

--- playbook/__main__.py
+++ playbook/__main__.py
@@ -1,21 +1,13 @@
 """An AWS Python Pulumi AWS Module"""
-import pulumi
 import pulumi_command
 
-k3s_server_ip = pulumi_command.local.Command(
-        "get_k3s_server_ip",
-        create="pulumi stack output k3s_server_public_ip"
-        )
+k3s_server_ip = pulumi_command.local.Command( "get_k3s_server_ip", create="pulumi stack output k3s_server_public_ip" )
+db_server_ip = pulumi_command.local.Command( "get_db_server_ip", create="pulumi stack output db_server_public_ip" )
 
-db_server_ip = pulumi_command.local.Command(
-        "get_db_server_ip",
-        create="pulumi stack output db_server_public_ip"
+render_inventory_cmd = pulumi_command.local.Command(
+        "Render",
+        create="python3 scripts/render.py hosts/ k3s_server_ip db_server_ip"
         )
-
-inventory_file = pulumi.Output.all(
-        db_server=db_server_ip,
-        k3s_server=k3s_server_ip
-        ).apply("python3 scripts/render.py hosts/ args['k3s_server']['stdout'] args['db_server']['stdout']")
 
 install_log_agent_cmd = pulumi_command.local.Command(
         "InstallAgent",
