09cec7de678cc0e35c678a2472a42c32bbf64460
Haitao Pan <manbuzhe2009@qq.com>
2023-03-24 14:05:39 +0800

    keycloak: add role and ci pipeline

---
+++ playbook/roles/keycloak/files/setup-keycloak.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export keycloak_db_password=$1
+export keycloak_ui_password=$2
+export domain=$3
+export secret=$4
+export namespace=$5
+
+cat > keycloak-vaules.yaml << EOF
+postgresql:
+  enabled: false
+ingress:
+  enabled: true
+  ingressClassName: "nginx"
+  hostname: keycloak.${domain}
+  tls: true
+  extraTls:
+  - hosts:
+      - keycloak.${domain}
+    secretName: $secret
+auth:
+  adminPassword: "$keycloak_ui_password"
+externalDatabase:
+  host: "postgresql.database.svc.cluster.local"
+  port: 5432
+  user: keycloak
+  database: keycloak
+  password: "$keycloak_db_password"
+EOF
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm upgrade --install keycloak bitnami/keycloak -n $namespace --create-namespace -f keycloak-vaules.yaml
--- playbook/roles/postgresql/files/setup-postgresql.sh
+++ playbook/roles/postgresql/files/setup-postgresql.sh
@@ -3,4 +3,4 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami || echo true
 helm repo up
 kubectl create ns database || echo true
-helm upgrade --install mysql bitnami/mysql -n database
+helm upgrade --install postgresql bitnami/postgresql -n database
