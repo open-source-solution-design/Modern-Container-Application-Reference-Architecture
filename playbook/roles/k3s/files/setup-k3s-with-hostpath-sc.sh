48a779ca8557045563e989a043555d8392a1159d
Haitao Pan <manbuzhe2009@qq.com>
2023-03-27 23:23:33 +0800

    k3s/files/setup-k3s-with-hostpath-sc.sh: update

--- playbook/roles/k3s/files/setup-k3s-with-hostpath-sc.sh
+++ playbook/roles/k3s/files/setup-k3s-with-hostpath-sc.sh
@@ -6,7 +6,6 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.24.7+k3s1 sh -s - \
 	--write-kubeconfig-mode 644                          \
 	--write-kubeconfig ~/.kube/config                    \
 	--data-dir=/opt/rancher/k3s                          \
-	--cni /opt/rancher/k3s/server/manifests/cilium.yaml  \
 	--kube-apiserver-arg service-node-port-range=0-50000
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
@@ -19,6 +18,12 @@ case `uname -m` in
 esac
 rm -rf helm.tar.gz* /usr/local/bin/helm || echo true 
 sudo wget --no-check-certificate https://mirrors.onwalk.net/tools/linux-${ARCH}/helm.tar.gz && sudo tar -xvpf helm.tar.gz -C /usr/local/bin/
+
+CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/master/stable.txt)
+curl -L --fail --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-linux-${ARCH}.tar.gz{,.sha256sum}
+sha256sum --check cilium-linux-${CLI_ARCH}.tar.gz.sha256sum && sudo tar xzvfC cilium-linux-${CLI_ARCH}.tar.gz /usr/local/bin && rm cilium-linux-${CLI_ARCH}.tar.gz{,.sha256sum}
+cilium install && cilium status --wait
+
 sudo chmod 755 /usr/local/bin/helm
 helm repo add artifact https://artifact.onwalk.net/chartrepo/k8s/ | echo true
 helm repo up
