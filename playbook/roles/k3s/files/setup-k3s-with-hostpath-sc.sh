f8a268752a85a80503d436fcde643a37f0fff604
Haitao Pan <manbuzhe2009@qq.com>
2023-03-29 22:09:28 +0800

    role/k3s: cni rollback to flannel

--- playbook/roles/k3s/files/setup-k3s-with-hostpath-sc.sh
+++ playbook/roles/k3s/files/setup-k3s-with-hostpath-sc.sh
@@ -1,14 +1,13 @@
+#!/bin/bash
+
+export k3s_public_ip=$1
+
 mkdir -pv /opt/rancher/k3s
 curl -sfL https://get.k3s.io | sh -s - \
-	--disable=traefik                                    \
-	--flannel-backend=none                               \
-	--disable-network-policy                             \
-	--disable-kube-proxy                                 \
+	--disable=traefik,servicelb                          \
 	--write-kubeconfig-mode 644                          \
 	--write-kubeconfig ~/.kube/config                    \
-	--node-external-ip 42.xx.xx.12                       \
-	--advertise-address 42.xx.xx.12                      \
-	--node-ip 192.168.1.1                                \
+	--node-external-ip $k3s_public_ip                    \
 	--data-dir=/opt/rancher/k3s                          \
 	--kube-apiserver-arg service-node-port-range=0-50000
 
@@ -24,18 +23,5 @@ rm -rf helm.tar.gz* /usr/local/bin/helm || echo true
 sudo wget --no-check-certificate https://mirrors.onwalk.net/tools/linux-${ARCH}/helm.tar.gz && sudo tar -xvpf helm.tar.gz -C /usr/local/bin/
 sudo chmod 755 /usr/local/bin/helm
 
-helm repo add cilium https://helm.cilium.io/
 helm repo add artifact https://artifact.onwalk.net/chartrepo/k8s/ | echo true
 helm repo up
-
-helm install cilium cilium/cilium --version 1.13.1 \
-	--namespace kube-system             \
-	--set bpf.masquerade=true           \
-	--set egressGateway.enabled=true    \
-	--set kubeProxyReplacement=strict   \
-	--set operator.replicas=1           \
-	--set devices=eth0                  \
-	--set k8sServiceHost=192.168.31.254 \
-	--set k8sServicePort=6443           \
-	--set ipv4NativeRoutingCIDR=192.168.31.254/32 \
-	--set l7Proxy=false
