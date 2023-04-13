a58cfd7678e0c82677f1d5bec5ca3312e3895b15
Haitao Pan <manbuzhe2009@qq.com>
2023-04-13 08:56:41 +0800

    create new ec2: itsm.onwalk.net

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -16,8 +16,8 @@ key_pair = aws.key_pair(resource_name="my_ssh_key", public_key=ssh_key)
 
 k3s_server = aws.ec2(
         arch      = 'amd64',
-        ec2_type  = 't3.large',
-        ec2_name  = 'k3s_server.onwalk.net',
+        ec2_type  = 't3.medium',
+        ec2_name  = 'itsm.onwalk.net',
         key_name  = key_pair,
         subnet_id = subnets[0],
         security_group_id = sg_id
