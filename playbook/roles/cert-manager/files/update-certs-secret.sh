7cf905bffa2c7d0b57b1bd41a115f20d3f33f814
Haitao Pan <manbuzhe2009@qq.com>
2023-04-14 11:13:22 +0800

    roles/cert-manager: fixed syntax problem

--- playbook/roles/cert-manager/files/update-certs-secret.sh
+++ playbook/roles/cert-manager/files/update-certs-secret.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export secret=$3
-export key_file=$1
-export cert_file=$2
-export namespace=$3
+export secret=$1
+export key_file=$2
+export cert_file=$3
+export namespace=$4
 
 kubectl create namespace $namespace || echo true
 kubectl delete secret tls $secret -n $namespace || echo true
