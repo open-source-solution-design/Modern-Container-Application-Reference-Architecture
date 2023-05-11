3c65f17286e5ad1c8914c8703b6537c3d248b3d2
Haitao Pan <manbuzhe2009@qq.com>
2023-05-11 16:56:16 +0800

    roles/gitlab: update

--- playbook/roles/gitlab/files/setup.sh
+++ playbook/roles/gitlab/files/setup.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+check_empty() {
+  if [ -z "$1" ]; then
+    echo "$2"
+    exit 1
+  fi
+}
+
+check_empty "$1" "Please provide a domain name as the first argument"
+check_empty "$2" "Please provide a namespace as the second argument"
+check_empty "$3" "Please provide a GitLab secret as the third argument"
+check_empty "$4" "Please provide a GitLab database secret as the fourth argument"
+check_empty "$5" "Please provide a GitLab SSO secret as the fifth argument"
+check_empty "$6" "Please provide a GitLab SMTP secret as the sixth argument"
+check_empty "$7" "Please provide a GitLab Redis secret as the seventh argument"
+
 domain=$1
 namespace=$2
 gitlab_secret=$3
-gitlab_sso_secret=$4
+gitlab_db_secret=$4
+gitlab_sso_secret=$5
+gitlab_smtp_secret=$6
+gitlab_redis_secret=$7
 
 cat > gitlab-values.yaml <<EOF
 global:
@@ -22,6 +40,42 @@ global:
       secretName: $gitlab_secret
   minio:
     enabled: true
+  gitaly:
+    persistence:
+      enabled: true
+  psql:
+    host: postgresql.database.svc.cluster.local
+    port: 5432
+    username: postgres
+    database: gitlab
+    password:
+      secret: $gitlab_db_secret
+      key: password
+  redis:
+    host: redis-master.redis.svc.cluster.local
+    port: 6379
+    password:
+      enabled: true
+      secret: $gitlab_redis_secret
+      key: password
+  appConfig:
+    email:
+      from: 'manbuzhe2009@qq.com'
+      display_name: GitLab-System
+    smtp:
+      tls: true
+      enabled: true
+      port: 465
+      domain: exmail.qq.com
+      address: smtp.exmail.qq.com
+      user_name: 'manbuzhe2009@qq.com'
+      password:
+        secret: $gitlab_smtp_secret
+        key: password
+      authentication: "login"
+      starttls_auto: false
+      openssl_verify_mode: "peer"
+      pool: true
   appConfig:
     omniauth:
       enabled: true
@@ -41,6 +95,7 @@ kas:
 nginx-ingress:
   enabled: false
 postgresql:
+  install: false
   metrics:
     enabled: false
 prometheus:
