b18f09f367c1437eed2ad624e6ca27fe4f7c8af0
Haitao Pan <manbuzhe2009@qq.com>
2023-05-28 16:58:28 +0800

    playbook/roles/k3s/files/setup-ingress-apisix-controller.sh: setup etcd.replicaCount=1

--- playbook/roles/k3s/files/setup-ingress-apisix-controller.sh
+++ playbook/roles/k3s/files/setup-ingress-apisix-controller.sh
@@ -2,12 +2,13 @@
 export ingress_ip=$1
 helm repo add apisix https://charts.apiseven.com || echo true
 helm repo update
-kubectl create ns ingress-apisix || echo true
+kubectl create ns ingress || echo true
 helm upgrade --install apisix apisix/apisix \
+  --set etcd.replicaCount=1                 \
   --set gateway.type=NodePort               \
   --set gateway.externalIPs[0]=$ingress_ip  \
   --set ingress-controller.enabled=true     \
-  --namespace ingress-apisix                \
-  --set ingress-controller.config.apisix.serviceNamespace=ingress-apisix \
+  --namespace ingress                       \
+  --set ingress-controller.config.apisix.serviceNamespace=ingress \
   --kubeconfig /etc/rancher/k3s/k3s.yaml
-kubectl get service --namespace ingress-apisix
+kubectl get service --namespace ingress
