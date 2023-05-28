67161323455174b4740409dacf2a59f84b7c0ac3
Haitao Pan <manbuzhe2009@qq.com>
2023-05-28 17:08:16 +0800

    roles/k3s/files/setup-cni-kubeovn.sh: update

--- playbook/roles/k3s/files/setup-cni-kubeovn.sh
+++ playbook/roles/k3s/files/setup-cni-kubeovn.sh
@@ -6,4 +6,4 @@ kubectl label node $node_name kubernetes.io/os=linux --overwrite
 kubectl label node $node_name kube-ovn/role=master --overwrite
 helm repo add kubeovn https://kubeovn.github.io/kube-ovn/
 helm repo up
-helm upgrade --install kube-ovn kubeovn/kube-ovn --set MASTER_NODES=${NodeIP}
+helm upgrade --install kube-ovn kubeovn/kube-ovn --set MASTER_NODES=${NodeIP} -n kube-system
