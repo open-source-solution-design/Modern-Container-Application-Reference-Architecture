89c7720aed284e1ae47db395863cd8d8bb6d4661
Haitao Pan <manbuzhe2009@qq.com>
2023-05-31 12:45:23 +0800

    playbook/roles/k3s/files/setup-cni-cilium.sh: add flannel clean opts

--- playbook/roles/k3s/files/setup-cni-kubeovn.sh
+++ playbook/roles/k3s/files/setup-cni-kubeovn.sh
@@ -9,6 +9,19 @@ modprobe iptable_nat
 
 rm -rvf /etc/cni/net.d/*
 
+# 移除cni命名空间
+ip netns show 2>/dev/null | grep cni- | xargs -r -t -n 1 ip netns delete
+# 移除cnio网卡
+ip link show 2>/dev/null | grep 'master cni0' | while read ignore iface ignore; do
+    iface=${iface%%@*}
+    [ -z "$iface" ] || ip link delete $iface
+done
+ip link delete cni0
+ip link delete flannel.1
+rm -rf /var/lib/cni/
+# 清理iptables
+iptables-save | grep -v KUBE- | grep -v CNI- | iptables-restore
+
 kubectl taint node $node_name node-role.kubernetes.io/control-plane:NoSchedule-
 kubectl label node $node_name kubernetes.io/os=linux --overwrite
 kubectl label node $node_name kube-ovn/role=master --overwrite
