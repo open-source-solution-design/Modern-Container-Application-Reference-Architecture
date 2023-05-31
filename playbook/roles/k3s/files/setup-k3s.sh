707a11929d7452c54b2d4db046264643867c6f4d
Haitao Pan <manbuzhe2009@qq.com>
2023-05-31 12:59:45 +0800

    playbook/roles/k3s/files/setup-k3s.sh: disable-kube-proxy

--- playbook/roles/k3s/files/setup-k3s.sh
+++ playbook/roles/k3s/files/setup-k3s.sh
@@ -18,8 +18,6 @@ function set_k3s_default()
     curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=$version sh -s - \
   	--disable=traefik,servicelb                          \
   	--cluster-domain=$cluster_domain                     \
-	--cluster-cidr=$pod_cidr                             \
-	--service-cidr=$svc_cidr                             \
   	--write-kubeconfig-mode 644                          \
   	--write-kubeconfig ~/.kube/config                    \
   	--data-dir=/opt/rancher/k3s                          \
@@ -29,8 +27,6 @@ function set_k3s_default()
     curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_VERSION=$version  INSTALL_K3S_MIRROR=cn sh -s - \
   	--disable=traefik,servicelb                          \
   	--cluster-domain=$cluster_domain                     \
-	--cluster-cidr=$pod_cidr                             \
-	--service-cidr=$svc_cidr                             \
   	--write-kubeconfig-mode 644                          \
   	--write-kubeconfig ~/.kube/config                    \
   	--data-dir=/opt/rancher/k3s                          \
@@ -46,6 +42,7 @@ function set_k3s_without_cni()
   if [ $? -eq 0 ]; then
     echo "当前主机在国际网络上"
     curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=$version sh -s - \
+	--disable-kube-proxy                                 \
 	--disable=traefik,servicelb                          \
 	--cluster-domain=$cluster_domain                     \
 	--cluster-cidr=$pod_cidr                             \
@@ -59,6 +56,7 @@ function set_k3s_without_cni()
   else
     echo "当前主机在大陆网络上"
     curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_VERSION=$version  INSTALL_K3S_MIRROR=cn sh -s - \
+	--disable-kube-proxy                                 \
 	--disable=traefik,servicelb                          \
 	--cluster-domain=$cluster_domain                     \
 	--cluster-cidr=$pod_cidr                             \
