79975099687234523d197f6dcc911af725b8a24e
Haitao Pan <manbuzhe2009@qq.com>
2023-06-01 20:14:06 +0800

    merge into k3s/files/reset-k3s.sh

--- playbook/roles/k3s/files/reset-k3s.sh
+++ playbook/roles/k3s/files/reset-k3s.sh
@@ -15,3 +15,18 @@ rm -fr /var/log/kube-ovn
 
 /usr/local/bin/k3s-uninstall.sh
 rm -rvf /opt/rancher/ /etc/rancher/ /var/lib/rancher/
+
+rm -rvf /etc/cni/net.d/*
+
+# 移除cni命名空间
+ip netns show 2>/dev/null | grep cni- | xargs -r -t -n 1 ip netns delete
+# 移除cnio网卡
+ip link show 2>/dev/null | grep 'master cni0' | while read ignore iface ignore; do
+    iface=${iface%%@*}
+    [ -z "$iface" ] || ip link delete $iface
+done
+ip link delete cni0
+ip link delete flannel.1
+rm -rf /var/lib/cni/
+# 清理iptables
+iptables-save | grep -v KUBE- | grep -v CNI- | iptables-restore
