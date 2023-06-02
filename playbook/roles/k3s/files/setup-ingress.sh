d957a3a66748cefbbdb391f4e7e708bea8d81bb4
Haitao Pan <manbuzhe2009@qq.com>
2023-06-02 11:36:53 +0800

    playbook/roles/k3s/files/setup-ingress.sh: enable discovery

--- playbook/roles/k3s/files/setup-ingress.sh
+++ playbook/roles/k3s/files/setup-ingress.sh
@@ -63,17 +63,32 @@ export ingress_ip=$1
 helm repo add apisix https://charts.apiseven.com || echo true
 helm repo update
 kubectl create ns ingress || echo true
-helm upgrade --install apisix apisix/apisix \
-  --set etcd.replicaCount=1                 \
-  --set gateway.type=NodePort               \
-  --set gateway.http.enabled=true           \
-  --set gateway.http.nodePort=80            \
-  --set gateway.tls.enabled=true            \
-  --set gateway.tls.nodePort=443            \
-  --set gateway.externalIPs[0]=$ingress_ip  \
-  --set ingress-controller.enabled=true     \
-  --namespace ingress                       \
-  --set ingress-controller.config.apisix.serviceNamespace=ingress \
-  --kubeconfig /etc/rancher/k3s/k3s.yaml
+cat > values.yaml << EOF
+ingress-controller:
+  enabled: true
+  config:
+    apisix:
+      serviceNamespace: ingress
+etcd:
+  replicaCount: 1
+gateway:
+  type: NodePort
+  http:
+    enabled: true
+    nodePort: 80
+  tls:
+    enabled: true
+    nodePort: 443
+  externalIPs:
+    - $ingress_ip
+admin:
+  enabled: true
+  type: NodePort
+  externalIPs: 
+    - $ingress_ip
+discovery:
+  enabled: true
+EOF
+helm upgrade --install apisix apisix/apisix --namespace ingress --kubeconfig /etc/rancher/k3s/k3s.yaml -f values.yaml
 kubectl get service --namespace ingress
 fi
