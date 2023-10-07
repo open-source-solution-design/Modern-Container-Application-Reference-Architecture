7d4fdaea5188bd092011414fc26f96caeaeacc44
Haitao Pan <manbuzhe2009@qq.com>
2023-10-07 10:53:12 +0800

    playbook/roles/cert-manager/files/certs_automated_issuance.sh: update

--- playbook/roles/cert-manager/files/certs_automated_issuance.sh
+++ playbook/roles/cert-manager/files/certs_automated_issuance.sh
@@ -15,25 +15,9 @@ rm -rvf /etc/ssl/${Domain}.* -f
 # Try to issue a certificate from ZeroSSL. If it fails, try Let's Encrypt.
   
 sh ~/.acme.sh/acme.sh --set-default-ca --server zerossl --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"; 
+cat ~/.acme.sh/${domain}_ecc/${domain}.cer > ${domain}.pem
+cat ~/.acme.sh/${domain}_ecc/ca.cer >> ${domain}.pem
+cat ~/.acme.sh/${domain}_ecc/${domain}.key > ${domain}.key
+sudo cp ${domain}.pem /etc/ssl/ -f && sudo cp ${domain}.key /etc/ssl/ -f
 
-if [[ ! -s "~/.acme.sh/${domain}_ecc/ca.cer" ]] || [[ ! -s "~/.acme.sh/${domain}_ecc/${domain}.cer" ]] || [[ ! -s "~/.acme.sh/${domain}_ecc/${domain}.key" ]]
-; then
-  echo "${domain}.pem and ${domain}.key is empty , Let's Try to issue a certificate with Let's Encrypt"
-  sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
-else
-  cat ~/.acme.sh/${domain}_ecc/${domain}.cer > ${domain}.pem
-  cat ~/.acme.sh/${domain}_ecc/ca.cer >> ${domain}.pem
-  cat ~/.acme.sh/${domain}_ecc/${domain}.key > ${domain}.key
-  sudo cp ${domain}.pem /etc/ssl/ -f && sudo cp ${domain}.key /etc/ssl/ -f
-fi
-
-if [[ ! -s "~/.acme.sh/${domain}_ecc/ca.cer" ]] || [[ ! -s "~/.acme.sh/${domain}_ecc/${domain}.cer" ]] || [[ ! -s "~/.acme.sh/${domain}_ecc/${domain}.key" ]]; then
-  echo "${domain}.pem and ${domain}.key must not be empty"
-  exit 1
-else
-  cat ~/.acme.sh/${domain}_ecc/${domain}.cer > ${domain}.pem
-  cat ~/.acme.sh/${domain}_ecc/ca.cer >> ${domain}.pem
-  cat ~/.acme.sh/${domain}_ecc/${domain}.key > ${domain}.key
-  sudo cp ${domain}.pem /etc/ssl/ -f && sudo cp ${domain}.key /etc/ssl/ -f
-  sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
-fi
+#sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
