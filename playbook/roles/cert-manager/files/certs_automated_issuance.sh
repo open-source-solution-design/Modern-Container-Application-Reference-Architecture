dd1988320546e39988505d43c4db245915346609
Haitao Pan <manbuzhe2009@qq.com>
2023-10-07 10:13:50 +0800

    roles/cert-manager/files/certs_automated_issuance.sh: update

--- playbook/roles/cert-manager/files/certs_automated_issuance.sh
+++ playbook/roles/cert-manager/files/certs_automated_issuance.sh
@@ -7,12 +7,6 @@ export Domain=$1
 export Ali_Key=$2
 export Ali_Secret=$3
 
-# Non-empty check for Ali_Key and Ali_Secret
-if [ -z "$Ali_Key" ] || [ -z "$Ali_Secret" ] || [ -z "$domain" ]; then
-    echo "Domain, Ali_Key and Ali_Secret must not be empty"
-    exit 1
-fi
-
 curl https://get.acme.sh | sh -s email=156405189@qq.com
 
 rm -rvf ${Domain}.* -f
@@ -27,8 +21,7 @@ fi
 cat ~/.acme.sh/${Domain}_ecc/${domain}.cer > ${Domain}.pem
 cat ~/.acme.sh/${Domain}_ecc/ca.cer >> ${Domain}.pem
 cat ~/.acme.sh/${Domain}_ecc/${Domain}.key > ${Domain}.key
-sudo cp ${Domain}.pem /etc/ssl/
-sudo cp ${Domain}.key /etc/ssl/
+sudo cp ${Domain}.pem /etc/ssl/ && sudo cp ${Domain}.key /etc/ssl/
 
 # Non-empty check for ${domain}.pem and ${Domain}.key
 if [ ! -s "${Domain}.pem" ] || [ ! -s "${Domain}.key" ]; then
