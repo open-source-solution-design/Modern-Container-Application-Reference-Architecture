73bb11f377fe5ceaf65fb817e815f205de6fba2a
Haitao Pan <manbuzhe2009@qq.com>
2023-10-07 11:05:11 +0800

    playbook/roles/cert-manager/files/certs_automated_issuance.sh: update

--- playbook/roles/cert-manager/files/certs_automated_issuance.sh
+++ playbook/roles/cert-manager/files/certs_automated_issuance.sh
@@ -15,9 +15,19 @@ rm -rvf /etc/ssl/${Domain}.* -f
 # Try to issue a certificate from ZeroSSL. If it fails, try Let's Encrypt.
   
 sh ~/.acme.sh/acme.sh --set-default-ca --server zerossl --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"; 
+if [ $? -eq 0 ]; then
+    echo "Certificate from zerossl successfully issued"
+else
+  sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
+  if [ $? -eq 0 ]; then
+      echo "Certificate from letsencrypt successfully issued"
+  else
+      echo "Command failed"
+      exit 1
+  fi   
+fi
+
 cat ~/.acme.sh/${domain}_ecc/${domain}.cer > ${domain}.pem
 cat ~/.acme.sh/${domain}_ecc/ca.cer >> ${domain}.pem
 cat ~/.acme.sh/${domain}_ecc/${domain}.key > ${domain}.key
 sudo cp ${domain}.pem /etc/ssl/ -f && sudo cp ${domain}.key /etc/ssl/ -f
-
-#sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
