59e140e5ed0c1a13435b0d4caa0f43e33707aefb
Haitao Pan <manbuzhe2009@qq.com>
2023-10-07 10:45:06 +0800

    playbook/roles/cert-manager/files/certs_automated_issuance.sh: update

--- playbook/roles/cert-manager/files/certs_automated_issuance.sh
+++ playbook/roles/cert-manager/files/certs_automated_issuance.sh
@@ -13,18 +13,27 @@ rm -rvf ${Domain}.* -f
 rm -rvf /etc/ssl/${Domain}.* -f
 
 # Try to issue a certificate from ZeroSSL. If it fails, try Let's Encrypt.
-if ! sh ~/.acme.sh/acme.sh --set-default-ca --server zerossl --issue --force --dns dns_ali -d ${Domain} -d "*.${Domain}"; 
-then
-    sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${Domain} -d "*.${Domain}"
+  
+sh ~/.acme.sh/acme.sh --set-default-ca --server zerossl --issue --force --dns dns_ali -d ${Domain} -d "*.${Domain}"; 
+
+if [[ ! -s "~/.acme.sh/${Domain}_ecc/ca.cer" ]] || [[ ! -s "~/.acme.sh/${Domain}_ecc/${domain}.cer" ]] || [[ ! -s "~/.acme.sh/${Domain}_ecc/${domain}.key" ]]
+; then
+  echo "${Domain}.pem and ${Domain}.key is empty , Let's Try to issue a certificate with Let's Encrypt"
+  sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${Domain} -d "*.${Domain}"
+else
+  cat ~/.acme.sh/${Domain}_ecc/${domain}.cer > ${Domain}.pem
+  cat ~/.acme.sh/${Domain}_ecc/ca.cer >> ${Domain}.pem
+  cat ~/.acme.sh/${Domain}_ecc/${Domain}.key > ${Domain}.key
+  sudo cp ${Domain}.pem /etc/ssl/ -f && sudo cp ${Domain}.key /etc/ssl/ -f
 fi
 
-cat ~/.acme.sh/${Domain}_ecc/${domain}.cer > ${Domain}.pem
-cat ~/.acme.sh/${Domain}_ecc/ca.cer >> ${Domain}.pem
-cat ~/.acme.sh/${Domain}_ecc/${Domain}.key > ${Domain}.key
-sudo cp ${Domain}.pem /etc/ssl/ && sudo cp ${Domain}.key /etc/ssl/
-
-# Non-empty check for ${domain}.pem and ${Domain}.key
-if [ ! -s "${Domain}.pem" ] || [ ! -s "${Domain}.key" ]; then
-    echo "${Domain}.pem and ${Domain}.key must not be empty"
-    exit 1
+if [[ ! -s "~/.acme.sh/${Domain}_ecc/ca.cer" ]] || [[ ! -s "~/.acme.sh/${Domain}_ecc/${domain}.cer" ]] || [[ ! -s "~/.acme.sh/${Domain}_ecc/${domain}.key" ]]; then
+  echo "${Domain}.pem and ${Domain}.key must not be empty"
+  exit 1
+else
+  cat ~/.acme.sh/${Domain}_ecc/${domain}.cer > ${Domain}.pem
+  cat ~/.acme.sh/${Domain}_ecc/ca.cer >> ${Domain}.pem
+  cat ~/.acme.sh/${Domain}_ecc/${Domain}.key > ${Domain}.key
+  sudo cp ${Domain}.pem /etc/ssl/ -f && sudo cp ${Domain}.key /etc/ssl/ -f
+  sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${Domain} -d "*.${Domain}"
 fi
