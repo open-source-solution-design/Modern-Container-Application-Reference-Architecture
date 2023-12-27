89d93d30f23e8bd293ec4018b2fa5ed3e9344194
Haitao Pan <manbuzhe2009@qq.com>
2023-12-27 18:07:50 +0800

    move setup kube-prometheus-stack into roles/k3s-addon

--- playbook/roles/observability-agent/files/setup.sh
+++ playbook/roles/observability-agent/files/setup.sh
@@ -36,39 +36,3 @@ kubectl label nodes $node prometheus=true --overwrite || echo true
 helm repo add stable https://charts.onwalk.net/ || echo true
 helm repo update
 helm upgrade --install observabilityagent stable/observabilityagent -n monitoring -f values.yaml
-
-cat > prometheus-agent-values.yaml << EOF
-global:
-  imageRegistry: "artifact.onwalk.net/base"
-prometheus:
-  agentMode: true
-  prometheusSpec:
-    remoteWrite:
-    - name: remote_prometheus
-      url: 'https://prometheus.svc.ink/api/v1/write'
-    retention: 24h
-    resources:
-      requests:
-        cpu: 200m
-        memory: 200Mi
-    podMonitorNamespaceSelector: { }
-    podMonitorSelector:
-      matchLabels:
-        app.kubernetes.io/component: monitoring
-defaultRules:
-  create: false
-grafana:
-  enabled: false
-prometheus-windows-exporter:
-  enabled: false
-alertmanager:
-  enabled: false
-EOF
-
-node_name=`kubectl get nodes | awk 'NR>1 {print $1}'`
-kubectl create namespace monitoring || echo true
-kubectl label nodes $node prometheus=true --overwrite || echo true
-helm repo add stable https://charts.onwalk.net/ || echo true
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack -n monitoring -f prometheus-agent-values.yaml
