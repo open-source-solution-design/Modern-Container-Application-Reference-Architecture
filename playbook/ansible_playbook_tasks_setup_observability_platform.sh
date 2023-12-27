596a52b971f1516d382c7cbed19e326b9d56e046
Haitao Pan <manbuzhe2009@qq.com>
2023-12-27 18:51:47 +0800

    renamed:  - playbook/pre_setup.sh -> playbook/ansible_playbook_devops_pre_setup.sh  - playbook/observability-platform-pre_setup.sh -> playbook/ansible_playbook_hosts_setup.sh  - init_observability-server -> playbook/ansible_playbook_tasks_setup_observability_platform.sh

---
+++ playbook/ansible_playbook_tasks_setup_observability_platform.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Function to check if a variable is empty
+check_empty() {
+    if [ -z "${!1}" ]; then
+        echo "$1 is empty. Aborting."
+        exit 1
+    fi
+}
+
+# List of variables to check
+variables=("domain")
+
+# Loop through variables and check if each one is empty
+for var in "${variables[@]}"; do
+    check_empty "$var"
+done
+
+cat > init_observability-server << EOF
+- name: setup observability server
+  hosts: all
+  user: root
+  become: yes
+  gather_facts: yes
+  tasks:
+    - include_role:
+        name: observability-server
+      vars:
+        group: master
+        update_secret: true
+        auto_issuance: false
+        namespace: monitoring
+        db_namespace: database
+        tls:
+          - secret_name: obs-tls
+            keyfile: /etc/ssl/${domain}.key
+            certfile: /etc/ssl/${domain}.pem
+    - include_role:
+        name: flagger-loadtester
+      vars:
+        group: master
+        update_secret: false
+        auto_issuance: false
+        namespace: loadtester
+        tls:
+          - secret_name: obs-tls
+            keyfile: /etc/ssl/${domain}.key
+            certfile: /etc/ssl/${domain}.pem
+EOF
