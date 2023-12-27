e197cb7f9e90cb52df7aad7df951786f3b918248
Haitao Pan <manbuzhe2009@qq.com>
2023-12-27 18:38:29 +0800

    files/setup-dns-provider.sh: set domainFilters with var:domain

--- playbook/roles/k3s-addon/files/setup-dns-provider.sh
+++ playbook/roles/k3s-addon/files/setup-dns-provider.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+# Function to check if a variable is empty
+check_empty() {
+    if [ -z "${!1}" ]; then
+        echo "$1 is empty. Aborting."
+        exit 1
+    fi
+}
+
+# List of variables to check ; Loop through variables and check if each one is empty
+
+variables=("ak" "sk" "domain")
+for var in "${variables[@]}"; do
+    check_empty "$var"
+done
+
 ak=$1
 sk=$2
+domain=$3
 
 cat > external-dns-values.yaml << EOF
 clusterDomain: admin.local
@@ -9,11 +25,7 @@ sources:
   - service
   - ingress
 domainFilters:
-  - onwalk.net
-  - svc.ink
-  - svc.plus
-  - svc-sit.ink
-  - svc-dev.ink
+  - $domain
 policy: upsert-only
 provider: alibabacloud
 alibabacloud:
