de253cc06eaaed6a672624aa92a43f4662478875
Haitao Pan <manbuzhe2009@qq.com>
2023-12-29 09:50:32 +0800

    roles/cert-manager/: update certs_automated_issuance.sh

--- playbook/roles/cert-manager/files/certs_automated_issuance.sh
+++ playbook/roles/cert-manager/files/certs_automated_issuance.sh
@@ -13,11 +13,11 @@ rm -fv /etc/ssl/${domain}.* -f
 # Try to issue a certificate from ZeroSSL. If it fails, try Let's Encrypt.
   
 curl https://get.acme.sh | sh -s email=156405189@qq.com
-sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
+sh ~/.acme.sh/acme.sh --set-default-ca --server zerossl --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
 if [ $? -eq 0 ]; then
     echo "Certificate from letsencrypt successfully issued"
 else
-  sh ~/.acme.sh/acme.sh --set-default-ca --server zerossl --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
+  sh ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt --issue --force --dns dns_ali -d ${domain} -d "*.${domain}"
   if [ $? -eq 0 ]; then
       echo "Certificate from zerossl successfully issued"
   else
