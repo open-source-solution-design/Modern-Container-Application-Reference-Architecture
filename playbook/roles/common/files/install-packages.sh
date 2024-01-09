9cd89bb9f8d383a0d531470863dad290273600c1
Haitao Pan <manbuzhe2009@qq.com>
2024-01-10 00:15:05 +0800

    remove unneed dependencies for observability-agent

--- playbook/roles/common/files/install-packages.sh
+++ playbook/roles/common/files/install-packages.sh
@@ -2,5 +2,6 @@
 
 export DEBIAN_FRONTEND=noninteractive
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt-get update && sudo apt-get install -y vault auditd
+sudo echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" > /etc/apt/sources.list.d/hashicorp.list
+sudo apt-get update
+sudo apt-get install -y vault auditd
